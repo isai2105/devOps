5fd6e467cf4727bf07d22efe44af4d09e11f5f48
Isai Ulate <isai.ulate@rivetlogic.com>
2021-08-31 16:41:50 -0600

    Apache Maven notes

---
+++ apache-maven/multi-project/com.vogella.maven.api/src/main/java/com/vogella/api/TaskService.java
@@ -0,0 +1,5 @@
+package com.vogella.api;
+
+public interface TaskService {
+
+}
---
+++ apache-maven/multi-project/com.vogella.maven.consumer/src/main/java/com/vogella/App.java
@@ -0,0 +1,12 @@
+package com.vogella;
+
+import com.vogella.api.TaskService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        TaskService taskService = null;
+    }
+}
